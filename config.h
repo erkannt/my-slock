3ad4e7b29d94d7d993545a05e9016c60ab11bc2e
Daniel Haarhoff <daniel@rknt.de>
2026-01-05 19:21:55 +0000

    Track lockscreen image from NASA/JPL-Caltech
    https://www.nasa.gov/image-article/curiosity-sends-holiday-postcard-from-mars/

--- config.h
+++ config.h
@@ -12,7 +12,7 @@ static const char *colorname[NUMCOLS] = {
 static const int failonclear = 1;
 
 /* Background image path, should be available to the user above */
-static const char* background_image = "/usr/share/wallpaper.png";
+static const char* background_image = "/usr/share/lockscreen.jpg";
 
 /* border width in pixels */
 static const int BORDER_WIDTH = 4;
